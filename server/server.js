8302a9ff1aa6b6f7727a60942d746150477daac8
Tanvi bansal <tanvibansal099@gmail.com>
2026-07-02 21:05:26 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -14,23 +14,7 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://task-flow-ai-bzyx-virid.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
