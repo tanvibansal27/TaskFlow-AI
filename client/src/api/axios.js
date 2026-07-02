fa48b10fe3853b0d2c22e6dceb14793ea6781f4b
Tanvi bansal <tanvibansal099@gmail.com>
2026-07-02 15:12:32 +0530

    Fix CORS for Vercel

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskflow-ai-9ln0.onrender.com",
+  baseURL: "https://taskflow-ai-9ln0.onrender.com/api",
 });
 
 export default API;
